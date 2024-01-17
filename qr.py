03d65d1cbc2e929f4d978de9464f01938c2cc160
medjadjiabdeldjali99l <abdeldjalilmedjadji907@gmail.com>
2024-01-17 14:39:37 +0100

    creat code with pythonn

--- qr.py
+++ qr.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image 
 
 #"Enter the URL of the data you want to create its QR code."
-data='url'
+data='www.linkedin.com/in/abdeldjalil-medjadji-b66671262'
 img =qrcode.make(data)
 img.save('qr.png')
 img=Image.open('qr.png')
