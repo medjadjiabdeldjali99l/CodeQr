7337a283ce71617cdddfa4489094684d0f7ad6e3
medjadjiabdeldjali99l <abdeldjalilmedjadji907@gmail.com>
2024-01-17 14:28:08 +0100

    creat code with pythonn

--- qr.py
+++ qr.py
@@ -2,7 +2,7 @@ import qrcode
 from PIL import Image 
 
 #"Enter the URL of the data you want to create its QR code."
-data='https://www.tiktok.com/@djadjooooooo?is_from_webapp=1&sender_device=pc'
+data='https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Faminestore09%3Figshid%3DMzMyNGUyNmU2YQ%253D%253D%26fbclid%3DIwAR33WjcDRJs9VCzwUQS9e2kQQ0jaPZwYnk8if7vO60YevJxJFxuGwYCXfW8&h=AT17MxgcUfrkXJiNHpdPLoT-d3J1g0--6sjN4LbaUROkqtojlLuohVnO-yGuiUcENJ7dmcO-fuATWOPQrEpCaTKPBO18MKALfz5ZvjDuY5279tB9eRjuJXERMArIY7vgRBUlnQ'
 img =qrcode.make(data)
 img.save('qr.png')
 img=Image.open('qr.png')
